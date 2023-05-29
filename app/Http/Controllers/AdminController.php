9a28e18d5b54b4c99dd94ab039b236d971732ddf
schallergergo <94720526+schallergergo@users.noreply.github.com>
2023-05-29 21:03:05 +0200

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -20,7 +20,7 @@ class AdminController extends Controller
     public function loginAsUser($userId){
         
         $this->isAdmin();
-        session()->put(["admin_id"=>$user->id]);
+        session()->put(["admin_id"=>$userId]);
         auth()->loginUsingId($userId);
         return redirect("/home");
     }
