a52ed1778faeb547533e85b4102a494bb86e0a65
Schaller Gergő <94720526+schallergergo@users.noreply.github.com>
2024-09-26 21:10:16 +0200

    Update ChampionshipController.php

--- app/Http/Controllers/ChampionshipController.php
+++ app/Http/Controllers/ChampionshipController.php
@@ -1,191 +1,126 @@
 <?php
 
-
-
 namespace App\Http\Controllers;
-
-
-
 use App\Models\Championship;
-
 use App\Models\Competition;
-
 use App\Models\Event;
-
 use App\Models\Start;
-
 use App\Http\Requests\StoreChampionshipRequest;
-
 use App\Http\Requests\UpdateChampionshipRequest;
-
 use App\Http\Controllers\ChampionshipType\PKDressageController;
 use App\Http\Controllers\ChampionshipType\PKDressage2OutOf3Controller;
-
 use App\Http\Controllers\ChampionshipType\PKShowJumpingController;
-
 use App\Http\Controllers\ChampionshipType\PKTeamDressageController;
-
 use App\Http\Controllers\ChampionshipType\PKTeamShowJumpingController;
-
 use App\Http\Controllers\ChampionshipType\PKClubController;
-
 use Illuminate\Support\Facades\Auth;
-
-
 
 class ChampionshipController extends Controller
 
 {
 
     /**
-
      * Display a listing of the resource.
-
      *
-
      * @return \Illuminate\Http\Response
-
      */
 
     public function index()
 
     {
 
-        $activeChampionships=Championship::orderBy("rank")->where("active",1)->paginate(20);
+        $activeChampionships = Championship::orderBy("rank")->where("active", 1)
+            ->paginate(20);
 
-        $closedChampionships=Championship::orderBy("rank")->where("active",0)->paginate(20);
+        $closedChampionships = Championship::orderBy("rank")->where("active", 0)
+            ->paginate(20);
 
-        return view("championship.index",["closedChampionships"=>$closedChampionships,"activeChampionships"=>$activeChampionships]);
+        return view("championship.index", ["closedChampionships" => $closedChampionships, "activeChampionships" => $activeChampionships]);
 
     }
 
-
-
     /**
-
      * Show the form for creating a new resource.
-
      *
-
      * @return \Illuminate\Http\Response
-
      */
 
     public function create()
 
     {
 
-        $this->authorize('create', [Championship::class]);
+        $this->authorize('create', [Championship::class ]);
 
         return view("championship.create");
 
     }
 
-
-
     /**
-
      * Store a newly created resource in storage.
-
      *
-
      * @param  \App\Http\Requests\StoreChampionshipRequest  $request
-
      * @return \Illuminate\Http\Response
-
      */
 
     public function store(StoreChampionshipRequest $request)
 
     {
 
-        $this->authorize('create', [Championship::class]);
+        $this->authorize('create', [Championship::class ]);
 
-        $data=$request->validated();
+        $data = $request->validated();
 
+        $newChampionship = \App\Models\Championship::create([
 
+        'championshipname' => $data["championshipname"],
 
-        $newChampionship=\App\Models\Championship::create([
+        'discipline' => $data["discipline"],
 
-            'championshipname' => $data["championshipname"],
+        'office' => Auth::user()->id,
 
-            'discipline' => $data["discipline"],
-
-            'office'=>Auth::user()->id,
-
-            'type'=>$data["type"],
+        'type' => $data["type"],
 
         ]);
 
         $newChampionship->rank = $newChampionship->id;
         $newChampionship->save();
 
-
-
         return redirect("/championship/edit/{$newChampionship->id}");
 
     }
 
-
-
     /**
-
      * Display the specified resource.
-
      *
-
      * @param  \App\Models\Championship  $championship
-
      * @return \Illuminate\Http\Response
-
      */
 
     public function show(Championship $championship)
 
-    {   
+    {
 
-        
+        if ($championship->type == "pkdressage") $controller = new PKDressageController();
 
+        if ($championship->type == "pkdressage2out3") $controller = new PKDressage2OutOf3Controller();
 
+        if ($championship->type == "pkshowjumping") $controller = new PKShowJumpingController();
 
+        if ($championship->type == "pkteamdressage") $controller = new PKTeamDressageController();
 
+        if ($championship->type == "pkteamshowjumping") $controller = new PKTeamShowJumpingController();
 
-
-
-        if ($championship->type=="pkdressage") $controller = new PKDressageController();
-        
-        if ($championship->type=="pkdressage2out3") $controller = new PKDressage2OutOf3Controller();
-
-        if ($championship->type=="pkshowjumping") $controller = new PKShowJumpingController();
-
-        if ($championship->type=="pkteamdressage") $controller = new PKTeamDressageController();
-
-        if ($championship->type=="pkteamshowjumping") $controller = new PKTeamShowJumpingController();
-
-        if ($championship->type=="pkclub") $controller = new PKClubController();
+        if ($championship->type == "pkclub") $controller = new PKClubController();
 
         return $controller->show($championship);
 
-
-
     }
 
-
-
-
-
-
-
     /**
-
      * Show the form for editing the specified resource.
-
      *
-
      * @param  \App\Models\Championship  $championship
-
      * @return \Illuminate\Http\Response
-
      */
 
     public function edit(Championship $championship)
@@ -194,210 +129,156 @@ class ChampionshipController extends Controller
 
         $this->authorize('update', $championship);
 
-        $competitions=Competition::where("discipline", $championship->discipline)->orderBYDesc("created_at")->get();
+        $competitions = Competition::where("discipline", $championship->discipline)
+            ->orderBYDesc("created_at")
+            ->get();
 
-        $eventIds=json_decode($championship->events);
+        $eventIds = json_decode($championship->events);
 
-        $addedEvents=array();
+        $addedEvents = array();
 
-        foreach($eventIds as $id) {
+        foreach ($eventIds as $id)
+        {
 
-            $event=Event::find($id);
+            $event = Event::find($id);
 
-            if ($event==null) $this->removeEventFromList($championship,$id);
+            if ($event == null) $this->removeEventFromList($championship, $id);
 
-            else $addedEvents[]=$event;
+            else $addedEvents[] = $event;
 
-
-
-            }
-
-
-
-
-
-
+        }
 
         return view("championship.edit",
 
-            [
+        [
 
-                "championship"=>$championship,
+        "championship" => $championship,
 
-                "competitions"=>$competitions,
+        "competitions" => $competitions,
 
-                "addedEvents"=>$addedEvents,
+        "addedEvents" => $addedEvents,
 
-            ]);
+        ]);
 
     }
 
-
-
     /**
-
      * Update the specified resource in storage.
-
      *
-
      * @param  \App\Http\Requests\UpdateChampionshipRequest  $request
-
      * @param  \App\Models\Championship  $championship
-
      * @return \Illuminate\Http\Response
-
      */
 
     public function update(UpdateChampionshipRequest $request, Championship $championship)
 
     {
 
-                $this->authorize('update', $championship);
-
-                $data=$request->validated();
-
-                $championship->update($data);
-
-                return redirect()->back();
-
-    }
-
-
-
-
-
-    public function addEvent(Championship $championship){
-
         $this->authorize('update', $championship);
 
-        $data=request();
+        $data = $request->validated();
 
-
-
-        $data=$data->validate([
-
-            'competition' => ["required"],
-
-            'event'=>["required",'integer',"min:0"],
-
-            ]);
-
-
-
-        $events=json_decode($championship->events);
-
-
-
-        if (!in_array($data["event"],$events)) {
-
-
-
-            $events[]=$data["event"];
-
-            $championship->events=json_encode($events);
-
-            $championship->save();
-
-
-
-        }
+        $championship->update($data);
 
         return redirect()->back();
 
     }
 
-
-
-     public function changeStatus(Championship $championship){
+    public function addEvent(Championship $championship)
+    {
 
         $this->authorize('update', $championship);
 
-        
+        $data = request();
+
+        $data = $data->validate([
+
+        'competition' => ["required"],
+
+        'event' => ["required", 'integer', "min:0"],
+
+        ]);
+
+        $events = json_decode($championship->events);
+
+        if (!in_array($data["event"], $events))
+        {
+
+            $events[] = $data["event"];
+
+            $championship->events = json_encode($events);
+
+            $championship->save();
+
+        }
+
+        return redirect()
+            ->back();
+
+    }
+
+    public function changeStatus(Championship $championship)
+    {
+
+        $this->authorize('update', $championship);
 
         $championship->active = !$championship->active;
 
         $championship->save();
 
-        return redirect()->back();
+        return redirect()
+            ->back();
 
     }
 
-    
-
-
-
-
-
-    public function removeEvent(Championship $championship){
+    public function removeEvent(Championship $championship)
+    {
 
         $this->authorize('update', $championship);
 
-        $data=request();
+        $data = request();
 
+        $data = $data->validate([
 
+        'event_id' => ["required", 'integer', "min:0"],
 
-        $data=$data->validate([
+        ]);
 
+        $events = json_decode($championship->events);
 
-
-            'event_id'=>["required",'integer',"min:0"],
-
-            ]);
-
-
-
-        $events=json_decode($championship->events);
-
-        $index=array_search($data["event_id"],$events);
-
- 
+        $index = array_search($data["event_id"], $events);
 
         unset($events[$index]);
 
-        $championship->events=json_encode(array_values($events));
+        $championship->events = json_encode(array_values($events));
 
         $championship->save();
 
-        return redirect()->back();
+        return redirect()
+            ->back();
 
     }
 
+    private function removeEventFromList(Championship $championship, int $id)
+    {
 
+        $events = json_decode($championship->events);
 
-    private function removeEventFromList(Championship $championship,int $id){
-
-        
-
-
-
-        $events=json_decode($championship->events);
-
-        $index=array_search($id,$events);
+        $index = array_search($id, $events);
 
         unset($events[$index]);
 
-               $championship->events=json_encode(array_values($events));
+        $championship->events = json_encode(array_values($events));
 
         $championship->save();
 
-
-
     }
-
-
-
-
 
     /**
-
      * Remove the specified resource from storage.
-
      *
-
      * @param  \App\Models\Championship  $championship
-
      * @return \Illuminate\Http\Response
-
      */
 
     public function destroy(Championship $championship)
@@ -411,8 +292,6 @@ class ChampionshipController extends Controller
         return redirect("/championship/index");
 
     }
-
-
 
 }
 
