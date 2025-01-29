40b44dafc3ac1ddf8315e89262351b461d830d6a
Schaller Gergő <94720526+schallergergo@users.noreply.github.com>
2025-01-30 00:30:11 +0100

    translation

--- resources/views/resultphoto/edit/photo.blade.php
+++ resources/views/resultphoto/edit/photo.blade.php
@@ -1,6 +1,6 @@
                                 <div class="card">
 
-                <div class="card-header">{{ __('Edit result photo') }}
+                <div class="card-header">{{ __('Image upload') }}
                 
 
                 
