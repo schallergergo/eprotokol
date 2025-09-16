3fdaf00f5a6153defdf1cb89a0d5593446cb8744
Schaller Gergő <94720526+schallergergo@users.noreply.github.com>
2025-09-16 14:21:53 +0200

    Update index.blade.php

--- resources/views/usage_log/index.blade.php
+++ resources/views/usage_log/index.blade.php
@@ -45,7 +45,7 @@
                                 <span class="badge bg-primary">{{ ucfirst($log->action) }}</span>
                             </td>
                             @if ($log->model == 'Result')
-                            <td> <a href="/result/show/{{$log->model_id}}"></a>{{ $log->model_id}}</td>
+                            <td> <a href="/result/show/{{$log->model_id}}">{{ $log->model_id}}</a></td>
                             @else 
                             <td>{{ $log->model_id}}</td>
                             @endif
